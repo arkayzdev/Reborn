d695fbb9cc52c1994f2b9a4e4440e6b68c6893c6
arkayzdev <franck.zhuang@hotmail.com>
2023-09-25 00:37:00 +0200

    scraping free proxy list

--- config.py
+++ config.py
@@ -1,1 +1,23 @@
-ips = ['141.11.158.172:18549']
+import requests
+from bs4 import BeautifulSoup
+import pprint
+
+url = f'https://free-proxy-list.net/'
+res = requests.get(url)
+soup = BeautifulSoup(res.text, 'html.parser')
+
+ips = soup.findAll('tr')
+
+available_ips = []
+
+for idx, ip in enumerate (ips):
+    https = ip.select('.hx')[0].getText()
+    if https == 'yes':
+        # print(len(ip))
+        for column, value in enumerate (ip):
+            print(value)
+            available_ips.append('ip': value[0])
+        break
+
+
+
