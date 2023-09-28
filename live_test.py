f05179ab1f11b95097e5a2ae779d2eea37df6f28
arkayzdev <franck.zhuang@hotmail.com>
2023-09-29 00:20:18 +0200

    Scraping with Playwright Framework

--- live_test.py
+++
@@ -1,9 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-res = requests.get('https://pinterest.com/search/pins/?q=gojo')
-soup = BeautifulSoup(res.text, 'html.parser')
-print(soup)
-
-with open("index.html", "w", encoding="utf-8") as file:
-    file.write(res.text)
--- pinterest.py
+++ pinterest.py
@@ -5,37 +5,34 @@ import time
 from config import Proxy
 
 class Scraper:
-    def pinterest_scraper(search):
+    def pinterest_scraper(search, proxy):
         scroll_num = 1
         sleepTimer = 1
-        url = f'https://pinterest.com/search/pins/?q={search}'
+        
         options = webdriver.ChromeOptions()
-        proxy = Proxy.get_proxy()
         options.add_experimental_option("excludeSwitches", ["enable-logging"])
-        options.add_argument(f'--proxy-server={proxy}')
+        # options.add_argument(f'--proxy-server={proxy}')
         driver = webdriver.Chrome(options=options)
-        # driver.set_page_load_timeout(60)
-        # try:
-        driver.get(url)
-        # print("URL successfully Accessed")
-        soup = BeautifulSoup(driver.page_source, 'html.parser')
-     
-        # except TimeoutException:
-        #     print("Page load Timeout Occured. Quiting !!!")
-        #     driver.quit()
-            
-            
 
+        url = f'https://pinterest.com/search/pins/?q={search}'
+        driver.get(url)
+
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
         for i in range (1, scroll_num):
             driver.execute_script("window.scrollTo(1, 1000000)")
             print('scroll-down')
             time.sleep(sleepTimer)
-
+    
         
-        print(soup.find('title').getText())
+        for link in soup.findAll('img'):
+            print(link.get('src'))
+              
+        
+        # print(driver.page_source)
         print(soup.getText())
         write_file(driver.page_source)
-        find_images(soup)
+    
+     
 
 
 
@@ -45,14 +42,9 @@ def write_file(page):
         file.write(page)
 
 
-def find_images(soup):
-    for link in soup.findAll('img'):
-        print(link.get('src'))
-
-
 if __name__ == "__main__":
     search = input("Keyworld: ")
-    Scraper.pinterest_scraper(search)
+    Scraper.pinterest_scraper(search, Proxy.get_proxy())
 
 
 
